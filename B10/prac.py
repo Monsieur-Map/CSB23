8854344caa0603bd7302523f1170dedb29e57187
Monsieur-Map <maianphuct24@gmail.com>
2025-08-03 11:32:09 +0700

    Buoi10

--- B10/prac.py
+++ B10/prac.py
@@ -22,7 +22,15 @@ def count_words (paragraph):
     return word_count
 
 p = """"
-For starters, orange cats are not a breed, like British shorthair or Siamese. Cats of many breeds, from Maine coons and munchkins to American bobtails and Siberians, can have orange colorations. Which perhaps makes it all the weirder that orange cat aficionados claim that orange cat behaviors transcend breed.
+But what kind of cat will they become? One possibility is that the cats of the future will be larger than today’s felines. 
+
+Continued contact with humans may also help cats grow friendlier. By nature, cats tend to be more solitary and standoffish than dogs, which have a long evolutionary history as pack animals rather than lone hunters. Ongoing contact with humans may have helped give friendlier cats a survival advantage, making them more likely to pass on more sociable genes to their offspring.
+
+Over time, this might have helped create a more relaxed pet—without ruining one of the most endearing parts of living with a cat: that when your kitty cuddles, you know it’s for real.
+
+When scientists compared the social behaviors and personalities of domestic cats with clouded leopards, snow leopards, African lions, and Scottish wildcats, they found that house cats weren’t exactly suited for group living. They tended to be domineering, neurotic, and impulsive—not exactly the traits you want in a friend.
+
+However, the personalities of house cats were most like those of the more sociable African lion, which has figured out a way to live in groups. This suggests that cats have the raw material to adapt to a more sociable future. And though large cat colonies may not be the same as lion prides, lions do show that cats can live side by side when the circumstances are right.
 """
 result = count_words(paragraph=p)
 # Hiển thị danh sách theo thứ tự chữ cái
